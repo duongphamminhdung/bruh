f84603b11e74e5f7b871963994fd0d45e86d8cdb
Duong Pham Minh Dung <duongphamminhdung@gmail.com>
2022-12-31 00:57:45 +0700

    websocket done

--- player/consumers.py
+++ player/consumers.py
@@ -3,14 +3,12 @@ import json
 
 class PlayerConsumer(WebsocketConsumer):
     
-    # def __init__(self):
-    #     room_id = self.scope["url_route"]["kwargs"]["room_id"]
-    #     self.room_id = room_id
-    #     print(room_id)
 
     def connect(self):
         self.accept()
         print("connected")
+        self.room_id = self.scope['url_route']['kwargs']['room_id']
+        print(self.room_id)
         
         return
     def receive(self, text_data=None, bytes_data=None):
