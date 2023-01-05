38f1b4687600d889069c5777fcd784e40dbcde61
Duong Pham Minh Dung <duongphamminhdung@gmail.com>
2023-01-06 00:32:58 +0700

    id

--- player/consumers.py
+++ player/consumers.py
@@ -1,13 +1,24 @@
 from channels.generic.websocket import WebsocketConsumer
+from .models import Room
+
 import json
+
+used_room_id = []
+for room in Room.objects.all():
+	used_room_id.append(room.room_id)
 
 class PlayerConsumer(WebsocketConsumer):
     
 
     def connect(self):
-        self.accept()
-        print("connected")
-        self.room_id = self.scope['url_route']['kwargs']['room_id']
+        self.room_id = int(self.scope['url_route']['kwargs']['room_id'])
+        created_id = [i['room_id'] for i in Room.objects.values('room_id')]
+        if self.room_id not in created_id:
+            self.close()
+            print("id not created")
+        else:
+            self.accept()
+            print("connected")
         print(self.room_id)
         
         return
--- src/settings.py
+++ src/settings.py
@@ -26,10 +26,12 @@ SECRET_KEY = 'django-insecure-@&$hb&@duh+=hpfuz-_37xup56a0-xc4=bhq$8hk&z+*qh=))v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['bruh-production-90b1.up.railway.app', 
-                 '127.0.0.1',
-                 'localhost',
-                '192.168.2.239'
+ALLOWED_HOSTS = [
+                'bruh-production-90b1.up.railway.app', 
+                '127.0.0.1',
+                'localhost',
+                '192.168.2.239',
+                '192.168.1.16',
                  ]
 
 
